74a98ea377dec75775efcd638f680bec37961ec9
Barry Oliver <i31770@gmail.com>
2016-08-30 16:01:09 -0400

    Start using .gitignore

---
+++ TripTracker/TripTracker/MapPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace TripTracker
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            //InitializeComponent ();
+            var map = new Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                MapType = MapType.Street,
+                IsShowingUser = true,
+                HeightRequest = 400,
+                WidthRequest = 400,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var nameEntry = new Entry
+            {
+                Text = "What is the name of this location?",
+                Placeholder = "Enter Name"
+            };
+
+            StackLayout stack = new StackLayout {
+                Spacing = 0,
+                HeightRequest = 400,
+                WidthRequest = 400
+            };
+
+            stack.Children.Add(map);
+            stack.Children.Add(nameEntry);
+            Content = stack;
+        }
+    }
+}
---
+++ TripTracker/TripTracker/TravelPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TripTracker
+{
+	public partial class TravelPage : ContentPage
+	{
+		public TravelPage ()
+		{
+            //InitializeComponent ();
+            var stack = new StackLayout
+            {
+                Spacing = 0,
+                HeightRequest = 400,
+                WidthRequest = 400
+            };
+
+            //stack.Children.Add();
+            Content = stack;
+        }
+	}
+}
